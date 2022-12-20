2252649ae91fb9aed6347208a9fa359395809ea9
John Thomas Pricone <jtpricone@gmail.com>
2022-12-20 12:46:43 -0500

    done

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> game = new List<string>()
+            {
+                "Gta 5", "Red Dead Redemption", "Fortnite", "Rainbow Six Siege"
+            };
+
+            game.OrderBy(x => x.Length).ToList().ForEach(Console.WriteLine);
+
+            //var lengthOfName = game.OrderBy(x => x.Length).ToList();
+            //foreach (var item in lengthOfName)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            
         }
     }
 }
